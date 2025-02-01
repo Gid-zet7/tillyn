240031c0c3c5110c3913c543430a86c3925af3f7
GideonAdonteng <adontenggideon57@gmail.com>
2025-01-31 19:54:31 -0800

    updates

--- components/dashboard/products.tsx
+++ components/dashboard/products.tsx
@@ -4,7 +4,7 @@ import { useGetProductsQuery } from "@/redux/slices/productsApiSlice";
 
 import ProductCard from "@/components/products/cards/ProductCard";
 import LoaderSimple from "@/components/Loader/Loader-simple/page";
-import { AlertDestructive } from "@/components/alert/AlertDestructive";
+import { AlertDestructive } from "../alert/AlertDestructive";
 
 export default function DashProducts() {
   const {
